a83092087716fbaa9f1def8fef15c87be4b90f34
MiralEmad <moraemadsamir700@gmail.com>
2026-08-25 00:28:04 +0300

    last commit

--- lib/grocery_app.dart
+++ lib/grocery_app.dart
@@ -19,7 +19,7 @@ class GroceryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home : HomeScreen(),
+      home : OnboardingScreen(),
     );
     
 }
